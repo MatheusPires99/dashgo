228dca484ef8cbd48d23219c66ed93dd6327876b
Matheus Pires <matheushenriquepires99@gmail.com>
2021-04-02 15:29:46 -0300

    feat: create UserList page

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,139 @@
+import {
+  Button,
+  Flex,
+  Heading,
+  Box,
+  Icon,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Checkbox,
+  Tbody,
+  Td,
+  Text,
+} from '@chakra-ui/react';
+import { RiAddLine, RiPencilLine } from 'react-icons/ri';
+
+import { Header, SideBar } from '../../components';
+
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+
+      <Flex as="main" w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <SideBar />
+
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+            </Heading>
+
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+            >
+              Criar novo usuário
+            </Button>
+          </Flex>
+
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" w="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Usuário</Th>
+                <Th>Data de cadastro</Th>
+                <Th w="8" />
+              </Tr>
+            </Thead>
+
+            <Tbody>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td px="6">
+                  <Box>
+                    <Text fontWeight="bold">Matheus Pires</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      matheushenriquepires99@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>04 de Abril, 2021</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td px="6">
+                  <Box>
+                    <Text fontWeight="bold">Matheus Pires</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      matheushenriquepires99@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>04 de Abril, 2021</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td px="6">
+                  <Box>
+                    <Text fontWeight="bold">Matheus Pires</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      matheushenriquepires99@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>04 de Abril, 2021</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
