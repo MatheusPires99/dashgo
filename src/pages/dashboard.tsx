9a4c5372d6d534bb5afa3af174aca43ad1bd87de
Matheus Pires <matheushenriquepires99@gmail.com>
2021-04-03 09:12:25 -0300

    feat: Dashboard responsive

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -7,7 +7,13 @@ export default function Dashboard() {
   return (
     <SimpleGrid flex="1" align="flex-start" gap="4" minChildWidth={320}>
       {DASHBOARD_CHARTS.map(chart => (
-        <Box key={chart.title} p="8" bg="gray.800" borderRadius="8" pb="4">
+        <Box
+          key={chart.title}
+          p={['6', '8']}
+          bg="gray.800"
+          borderRadius="8"
+          pb="4"
+        >
           <Text fontSize="lg" mb="4">
             {chart.title}
           </Text>
