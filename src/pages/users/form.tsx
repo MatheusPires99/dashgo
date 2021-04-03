07528a062e041a5c88bf086b68325eef4c3760fa
Matheus Pires <matheushenriquepires99@gmail.com>
2021-04-03 09:32:46 -0300

    feat: UserForm responsive

--- src/pages/users/form.tsx
+++ src/pages/users/form.tsx
@@ -5,14 +5,14 @@ import { Input } from '../../components/Form';
 
 export default function UserForm() {
   return (
-    <FormWrapper title="     Criar usuário">
-      <VStack spacing="8">
-        <SimpleGrid minChildWidth={240} spacing="8" w="100%">
+    <FormWrapper title="Criar usuário">
+      <VStack spacing={['6', '8']}>
+        <SimpleGrid minChildWidth={240} spacing={['6', '8']} w="100%">
           <Input name="name" label="Nome completo" />
           <Input name="email" type="email" label="E-mail" />
         </SimpleGrid>
 
-        <SimpleGrid minChildWidth={240} spacing="8" w="100%">
+        <SimpleGrid minChildWidth={240} spacing={['6', '8']} w="100%">
           <Input name="password" type="password" label="Senha" />
           <Input
             name="password_confirmation"
