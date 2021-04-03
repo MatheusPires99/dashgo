aedf7360efc8c0484c22dbd92f24251610ce0a79
Matheus Pires <matheushenriquepires99@gmail.com>
2021-04-03 10:40:39 -0300

    feat: add react-hook-form lib and setup SignIn

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,47 @@
-import { Button, Stack } from '@chakra-ui/react';
+import { Button, Stack, Flex } from '@chakra-ui/react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
 import { Input } from '../components/Form';
 
-export default function SignIn() {
-  return (
-    <>
-      <Stack spacing="4">
-        <Input name="email" type="email" label="E-mail" />
+type SignInFormData = {
+  email: string;
+  password: string;
+};
 
-        <Input name="password" type="password" label="Senha" />
+export default function SignIn() {
+  const { handleSubmit, register, formState } = useForm();
+
+  const handleSignIn: SubmitHandler<SignInFormData> = value => {
+    console.log(value);
+  };
+
+  return (
+    <Flex as="form" onSubmit={handleSubmit(handleSignIn)} flexDir="column">
+      <Stack spacing="4">
+        <Input
+          name="email"
+          type="email"
+          label="E-mail"
+          {...register('email')}
+        />
+
+        <Input
+          name="password"
+          type="password"
+          label="Senha"
+          {...register('password')}
+        />
       </Stack>
 
-      <Button type="submit" mt="6" colorScheme="pink" size="lg">
+      <Button
+        type="submit"
+        mt="6"
+        colorScheme="pink"
+        size="lg"
+        isLoading={formState.isSubmitting}
+      >
         Entrar
       </Button>
-    </>
+    </Flex>
   );
 }
