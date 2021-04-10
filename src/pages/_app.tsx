479b7e69f4522c0d81542067215b698bf29e1cd9
Matheus Pires <matheushenriquepires99@gmail.com>
2021-04-10 10:51:58 -0300

    chore: add and setup miragejs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,11 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from '../styles/theme';
 import { AuthLayout, DefaultLayout } from '../components/_layouts';
 import { SidebarDrawerProvider } from '../hooks';
+import { makeServer } from '../services';
+
+if (process.env.NODE_ENV === 'development') {
+  makeServer();
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   const { asPath } = useRouter();
