30daeb1c697f44d4561f38bfa97f7369aea1f229
Matheus Pires <matheushenriquepires99@gmail.com>
2021-04-11 00:30:41 -0300

    chore: remove user-list file

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,3 @@
 export { default as SIDE_BAR_NAVIGATION } from './sidebar-navigation';
 export { default as DASHBOARD_CHARTS } from './dashboard-charts';
 export { default as PAGINATION } from './pagination';
-export { default as USER_LIST } from './user-list';
