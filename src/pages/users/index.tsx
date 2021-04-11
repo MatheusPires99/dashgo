dea131e11b4398b99d459a9aa6f7e8ba67af93c9
Matheus Pires <matheushenriquepires99@gmail.com>
2021-04-11 00:30:02 -0300

    feat: use axios to fetch data

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -16,27 +16,33 @@ import { RiPencilLine } from 'react-icons/ri';
 
 import { TableWrapper } from '../../components';
 import { User } from '../../types';
+import { api } from '../../services';
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery('users', async () => {
-    const response = await fetch('/api/users');
-    const responseData = await response.json();
+  const { data, isLoading, error } = useQuery<User[]>(
+    'users',
+    async () => {
+      const response = await api.get('users');
 
-    const users = responseData.users.map(user => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric',
-        }),
-      };
-    });
+      const users = response.data.users.map(user => {
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+            day: '2-digit',
+            month: 'long',
+            year: 'numeric',
+          }),
+        };
+      });
 
-    return users;
-  });
+      return users;
+    },
+    {
+      staleTime: 1000 * 5,
+    },
+  );
 
   const isWideVersion = useBreakpointValue({
     base: false,
